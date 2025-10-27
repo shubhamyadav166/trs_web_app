b3c6689f8be5c95a2905b11bf945d48c9396fe08
Shubham yadav <shubhamyadav1662000@gmail.com>
2025-10-27 20:32:55 +0530

    header and footer added in app

--- trs_web_app/src/App.jsx
+++ trs_web_app/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Footer from './componensts/Footer'
+import Navbar from './componensts/Navbar'
 
 function App() {
 
 
   return (
     <>
+      <Navbar />
+      <h1>Welcome At TRS Infraventures</h1>
+      <Footer />
     </>
   )
 }
